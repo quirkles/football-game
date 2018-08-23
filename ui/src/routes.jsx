dfab09d84d795f7f8c5f4546e0b8f6ea80bc16ab
jwellwood <joemadriddev@gmail.com>
2018-08-23 18:42:43 +0200

    add login route

--- ui/src/routes.jsx
+++ ui/src/routes.jsx
@@ -1,22 +1,27 @@
-import HelloWorld from './components/HelloWorld'
-import App from './components/App'
-import NotFound from './components/NotFound'
+import HelloWorld from './components/HelloWorld';
+import App from './components/App';
+import NotFound from './components/NotFound';
+import Login from './components/Login';
 
 const routes = [
-    {
-        path: '/',
-        component: App,
-        routes: [
-            {
-                path: '/hello-world',
-                component: HelloWorld,
-            },
-            {
-                path: '*',
-                component: NotFound,
-            }
-        ],
-    },
+  {
+    path: '/',
+    component: App,
+    routes: [
+      {
+        path: '/hello-world',
+        component: HelloWorld,
+      },
+      {
+        path: '*',
+        component: NotFound,
+      },
+      {
+        path: '/login',
+        component: Login,
+      },
+    ],
+  },
 ];
 
 export default routes;
